5ee8b2940a440f7c3b4d8b8ab2e88deea27a23de
Connor McCarthy and Meria Thomas <meria@>
2018-08-09 11:32:14 -0700

    add html and css

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,6 @@ $(document).ready(function(){
     var gender = $('input[name=gender]:checked').val();
     var hobbies = $("select#hobbies").val();
 
-    if(gender === "female" && hobbies === "hiking"){
-      $(".vanessa").show();
-      $(".zac").hide();
-      $(".brutal").hide();
-
-    }
-    else if(gender === "male" && hobbies ==="eating"){
-      $(".zac").show();
-      $(".vanessa").hide();
-      $(".brutal").hide();
-    }
-
-    else{
-      $(".brutal").show();
-      $(".zac").hide();
-      $(".vanessa").hide();
-    }
-
     event.preventDefault();
   });
 });
