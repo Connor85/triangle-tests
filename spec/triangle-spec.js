65a840d359e8eeef6171c55487c58bd885a04956
Connor McCarthy and David Zhu <david@>
2018-10-09 10:17:15 -0700

    pass 3 of 4 specs

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -9,4 +9,19 @@ describe('Triangle', function() {
     expect(triangle.side3).not.toEqual(6);
   });
 
+  it('should correctly determine whether the three sides cannnot be made into a triangle', function(){
+    var notTriangle = new Triangle(2,2,500);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+
+  it('should correctly determine if a triange is equalaterial', function(){
+    var equalTriangle = new Triangle (5,5,5);
+    expect(equalTriangle.checkType()).toEqual("an equilateral triangle");
+  });
+
+  it('should correctly determine if a triangle is isoceles',function(){
+    var isocelesTriangle = new Triangle(2,2,4);
+    expect(isocelesTriangle.checkType()).toEqual("an isosceles triangle");
+  });
+
 });
